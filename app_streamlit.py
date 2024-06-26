1980c9aae07af62f0fc2cdc6b4c6d10d04d738e4
Riris Iyut Roito Siregar <ririsiyut03@gmail.com>
2024-06-26 10:16:31 +0700

    update 3

--- app_streamlit.py
+++ app_streamlit.py
@@ -5,13 +5,14 @@ from dotenv import load_dotenv
 
 if os.path.isfile('.env'):
     load_dotenv()
+API_FLASK_CONVERT = "http://127.0.0.1:5000/convert" if os.getenv("API_FLASK_CONVERT") is None else os.getenv("API_FLASK_CONVERT")
 
 st.title("Permainan Tebak Angka Ganjil atau Genap")
 
 angka_pertama = st.number_input('Masukkan angka pertama:', min_value=0, value=0)
 angka_kedua = st.number_input('Masukkan angka kedua:', min_value=0, value=0)
 
-API_FLASK_CONVERT = "http://127.0.0.1:5000/convert" if os.getenv("API_FLASK_CONVERT") is None else os.getenv("API_FLASK_CONVERT")
+
 
 if st.button("Tampilkan Hasil"):
     response = requests.post(
