3ea56a8fd4ef19d98528a5d1a4e6f838511aa219
Riris Iyut Roito Siregar <ririsiyut03@gmail.com>
2024-06-26 10:08:56 +0700

    update 1

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,23 +1,21 @@
 import streamlit as st
 import requests
-import os
-from dotenv import load_dotenv
-
-if os.path.isfile('.env'):
-    load_dotenv()
-
-API_FLASK_CONVERT = "https://ed0c-103-82-14-56.ngrok-free.app/convert" if os.getenv("API_FLASK_CONVERT") is None else os.getenv("API_FLASK_CONVERT")
 
 st.title("Permainan Tebak Angka Ganjil atau Genap")
 
 angka_pertama = st.number_input('Masukkan angka pertama:', min_value=0, value=0)
 angka_kedua = st.number_input('Masukkan angka kedua:', min_value=0, value=0)
 
-if st.button("Tampilkan Hasil"):
-    response = requests.post(
-        API_FLASK_CONVERT,
-        json={"angka_pertama": angka_pertama, "angka_kedua": angka_kedua}
-    )
+if st.button("convert"):
+    try:
+
+        response = requests.post(
+            "https://ed0c-103-82-14-56.ngrok-free.app/convert",
+            json={"angka_pertama": angka_pertama, "angka_kedua": angka_kedua}
+        )
+    except Exception as e :
+        print("Exception", e)
+        st.error("Error in conversion")
     
     if response.status_code == 200:
         result = response.json()
